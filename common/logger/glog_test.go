ce0a2b6ef386a5c217fad38410042751f5bee2cd
mayb <mayb@rp-pet.cn>
2019-12-25 09:18:11 +0800

    update

---
+++ common/logger/glog_test.go
@@ -0,0 +1,39 @@
+
+package glog
+
+import "testing"
+
+func TestVerbose_Info(t *testing.T) {
+	type args struct {
+		args []interface{}
+	}
+	tests := []struct {
+		name string
+		v    Verbose
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.v.Info(tt.args.args...)
+		})
+	}
+}
+
+func TestInfo(t *testing.T) {
+	type args struct {
+		args []interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			Info(tt.args.args...)
+		})
+	}
+}
